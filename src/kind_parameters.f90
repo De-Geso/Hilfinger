152304059ef462d5d27168275d1056135e1488fa
Brad Friesen <bradleyfriesen17@gmail.com>
2024-12-03 18:18:44 -0500

    Merged kind_parameters.f90. Fixed incorrect comment.

--- src/kind_parameters.f90
+++ src/kind_parameters.f90
@@ -16,7 +16,7 @@ integer, parameter :: i1 = selected_int_kind(2)
 integer, parameter :: i2 = selected_int_kind(4)
 ! Length of default integers, range -2^31 to 2^31-1; 32 bits
 integer, parameter :: i4 = selected_int_kind(9)
-! Long length for integers, range -2^7 to 2^7-1; 64 bits
+! Long length for integers, range -2^63 to 2^63-1; 64 bits
 integer, parameter :: i8 = selected_int_kind(18)
 
 end module kind_parameters
