bee94ecba83f5bfba554225bff80c98bc84d4bff
Brad Friesen <bradleyfriesen17@gmail.com>
2025-04-21 18:27:15 -0400

    Trying to make general covariance balance checker. I think it's working, currently testing.

--- gillespie_inversion/src/inversion_cascade_3D.f90
+++ gillespie_inversion/src/inversion_cascade_3D.f90
@@ -16,13 +16,19 @@ real(dp), parameter :: eps=tiny(eps)
 integer, parameter :: event_min = 10**6
 
 ! System parameters ====================================================
-real(dp), parameter :: lmbda = 1._dp
-real(dp), parameter :: beta = 1._dp
-real(dp), parameter :: k = 10._dp
-real(dp), parameter :: n = 2._dp
-real(dp), parameter :: c = 0._dp
-integer, parameter, dimension(2) :: burst = [1, -1]
-integer, parameter, dimension(2) :: abund_update = [burst(1), burst(2)]
+real(dp), parameter, dimension(3) :: lmbda = 10._dp
+real(dp), parameter, dimension(3) :: beta = 1._dp
+real(dp), parameter, dimension(3) :: k = 10._dp
+real(dp), parameter, dimension(3) :: n = 2._dp
+real(dp), parameter, dimension(3) :: c = 0._dp
+integer, parameter, dimension(3,6) :: burst = &
+	reshape((/5, 0, 0, &
+			-1, 0, 0, &
+			0, 1, 0, &
+			0, -1, 0, &
+			0, 0, 1, &
+			0, 0, -1/), shape(burst))
+integer, parameter, dimension(3,6) :: abund_update = burst
 
 ! Variables ============================================================
 ! Timers and counters
@@ -31,20 +37,21 @@ integer, allocatable :: visits(:)
 integer, allocatable :: exits(:,:)
 ! Stats/calculated
 real(dp), allocatable :: r_infer(:,:)
-type(OnlineCovariance) :: cov_balance, x_cov
-real(dp) :: x_mean, r_mean(2)
+type(OnlineCovariance) :: online_cov_balance(3,3), online_x_cov(3)
+real(dp) :: cov_balance(3,3), x_cov(3)
+real(dp) :: x_mean(3), r_mean(6)
 ! Gillespie
-real(dp) :: propensity(2), roll
-integer :: x=0, x_last, event_count(2)=0, event
+real(dp) :: propensity(6), roll
+integer :: x(3)=0, event_count(6)=0, event
 ! Other
-integer :: i, nseed
+integer :: i, j, nseed
 integer, allocatable :: rseed(:)
 
 ! Hashmap ==============================================================
 type :: state_data
 	real(dp) :: time_spent
 	integer :: visit_count
-	integer :: exit_count(2)
+	integer :: exit_count(6)
 end type state_data
 type(chaining_hashmap_type) :: map
 type(key_type) :: key
@@ -67,13 +74,20 @@ call random_seed(get=rseed)
 		
 do while (minval(event_count) < event_min)
 	! Update the propensity before taking a Gillespie step
-	call update_propensity(propensity, x)
+	! call update_propensity(propensity, x)
+	propensity = rates(x)
 	! Get timestep and event from Gillespie algorithm
 	call gillespie_iter(tstep, event, propensity)
 	
 	! Online calculation of mean and covariance
-	call cov_balance%update(real(x,dp), abs(burst(2))*propensity(2) - abs(burst(1))*propensity(1), tstep)
-	call x_cov%update(real(x,dp), real(x,dp), tstep)
+	do i = 1,3
+		call online_x_cov(i)%update(real(x(i),dp), real(x(i),dp), tstep)
+		do j = 1,3
+			call online_cov_balance(i,j)%update(&
+				real(x(i),dp), &
+				abs(burst(j,2*j))*propensity(2*j) - abs(burst(j,j))*propensity(j), tstep)
+		end do
+	end do
 	
 	! Update hashmap
 	call set(key, [x])
@@ -81,12 +95,14 @@ do while (minval(event_count) < event_min)
 	
 	! Update state of system for next step
 	t = t + tstep
-	x = x + abund_update(event)
+	x = x + abund_update(:,event)
 	event_count(event) = event_count(event) + 1
 end do
 
+! Get all the keys (states visited)
 call map%get_all_keys(keys)
 
+! Use retrieved keys and values to infer rates
 allocate(r_infer(size(keys),2))
 do i = 1, size(keys)
 	call map%get_other_data(keys(i), retrieved_values)
@@ -97,14 +113,22 @@ do i = 1, size(keys)
 	end select
 end do
 
-x_mean = x_cov%mean_x
+! Stats stuff for validity of simulation, i.e. check covariance balance
+x_mean = online_x_cov%mean_x
 r_mean = event_count/t
+do i = 1,3
+	x_cov(i) = online_x_cov(i)%get_cov()
+do j = 1,3
+	cov_balance(i,j) = online_cov_balance(i,j)%get_cov()
+end do
+end do
 
-call dump()
 write(*,*) "x mean:", x_mean
-write(*,*) "x covariance:", x_cov%get_cov()
+write(*,*) "x covariance:", x_cov
 write(*,*) "r mean:", r_mean
-call check_covariance_balance(cov_balance%get_cov(), x_mean, r_mean, burst, "arithmetic")
+
+call check_covariance_balance(3, cov_balance, x_mean, r_mean, burst, "arithmetic")
+call dump()
 
 
 contains
@@ -144,50 +168,70 @@ subroutine update_hashmap(this, key, dt, r_channel)
 end subroutine
 
 
-subroutine check_covariance_balance(cov, x_avg, r_avg, burst, change_method)
+subroutine check_covariance_balance(n, cov, x_avg, r_avg, burst, change_method)
 	character(len=*), intent(in) :: change_method
-	real(dp), intent(in) :: cov, x_avg, r_avg(2)
-	integer, intent(in) :: burst(2)
-	real(dp) :: rel_change, U, D, tau, s, flux_avg(2)
+	real(dp), intent(in) :: cov(n,n), x_avg(n), r_avg(:)
+	integer, intent(in) :: n, burst(n,2*n)
+	real(dp) :: rel_change, U(n,n), D(n,n), tau(n), s(n,n), flux_avg(n,2)
+	integer :: i, j, k
 	
-	flux_avg = abs(r_avg * burst)
 	
-	tau = 1._dp * x_avg / flux_avg(2)
-	s = 0.5_dp * (burst(1) - burst(2))
-		
-	U = 1._dp/tau * cov / x_avg / (sum(flux_avg)/size(flux_avg))
-	D = 2._dp/tau * s / x_avg
-	rel_change = relative_change(2._dp*U, D, change_method)
+	! Get fluxes
+	flux_avg = 0
+	do i = 1, n
+		do k = 1, size(r_avg)
+			if (burst(i,k) > 0) then
+				flux_avg(i,1) = flux_avg(i,1) + r_avg(k)*abs(burst(i,k))
+			elseif (burst(i,k) < 0) then
+				flux_avg(i,2) = flux_avg(i,2) + r_avg(k)*abs(burst(i,k))
+			end if
+		end do
+	end do
+	write(*,*) "flux: ", flux_avg
 	
-	write(*,*) change_method, " relative change between U+U^T and D:", rel_change
-	write(*,*) "2U:", 2._dp*U, "D:", D
+	! Get lifetimes
+	tau = 1._dp * x_avg / flux_avg(:,2)
+	write(*,*) "tau: ", tau
+	
+	! Get step sizes
+	s = 0._dp
+	do i = 1, n
+	do j = 1, n
+	do k = 1, size(r_avg)
+		s(i,j) = s(i,j) + (r_avg(k)*abs(burst(i,k))/sum(r_avg(:)*abs(burst(i,:))) &
+			* abs(burst(j,k)) * sign(1, burst(i,k)*burst(j,k)))
+	end do
+	end do
+	end do
+	print *, s
+	
+	write(*,*) change_method, " relative change between U(i,j)+U(j,i) and D(i,j):"
+	do i = 1, n
+	do j = 1, n
+		if (s(i,j) > eps) then
+			U(i,j) = 1._dp/tau(j) * cov(i,j)/(x_avg(i)*0.5*(flux_avg(j,1)+flux_avg(j,2)))
+			D(i,j) = s(i,j)/tau(i)/x_avg(j) + s(j,i)/tau(j)/x_avg(i)
+			rel_change = relative_change(U(i,j)+U(j,i), D(i,j), change_method)
+			write(*,*) "i:", i, "j:", j, "Relative change:", rel_change, "U+U^T: ", U(i,j)+U(j,i), "D:", D(i,j)
+		end if
+	end do
+	end do
 end subroutine
 
 
-pure function Rin(x) result(f)
-	integer, intent(in) :: x
-	real(dp) :: f
+pure function rates(x) result(r)
+	integer, intent(in) :: x(3)
+	real(dp) :: r(6)
 	
-	! Linear system
-	f = 1._dp * lmbda
+	r(1) = 1._dp * lmbda(1)
+	r(2) = 1._dp * x(1) * beta(1)
+	
+	r(3) = 1._dp * lmbda(2)
+	r(4) = 1._dp * x(2) * beta(2)
+	
+	r(5) = 1._dp * lmbda(3)
+	r(6) = 1._dp * x(3) * beta(3)
 end function
-
-
-pure function Rout(x) result(f)
-	integer, intent(in) :: x
-	real(dp) :: f
-	
-	f = 1._dp * x * beta
-end function
-
-
-subroutine update_propensity(prop, x)
-	integer, intent(in) :: x
-	real(dp), dimension(2), intent(out) :: prop
-	
-	prop(1) = Rin(x)
-	prop(2) = Rout(x)
-end subroutine
 
 
 subroutine dump()
@@ -198,17 +242,17 @@ subroutine dump()
 
 	print '("Number of keys in the hashmap = ", I0)', size(keys)
 
-	do i = 1, size(keys)
-		call set(key, [i-1])
-		call map%get_other_data(key, retrieved_values)
-		select type (retrieved_values)
-		type is (state_data)
-			print '("State ", I0, " = ", F20.12, " ", I0, " ", I0, " ", I0)', &
-				i-1, retrieved_values%time_spent/t, retrieved_values%visit_count, retrieved_values%exit_count
-			call get(keys(i), test)
-			write(*,*) i, test
-		end select		
-	end do
+!	do i = 1, size(keys)
+!		call set(key, [i-1])
+!		call map%get_other_data(key, retrieved_values)
+!		select type (retrieved_values)
+!		type is (state_data)
+!			print '("State ", I0, " = ", F20.12, " ", I0, " ", I0, " ", I0)', &
+!				i-1, retrieved_values%time_spent/t, retrieved_values%visit_count, retrieved_values%exit_count
+!			call get(keys(i), test)
+!			write(*,*) i, test
+!		end select		
+!	end do
 
 	
 	headers(1) = "x"
@@ -259,23 +303,23 @@ subroutine dump()
 		trim(headers(8)), &
 		trim(headers(9))
 		
-	do i = 1, size(keys)
-!		call set(key, keys(i))
-		call map%get_other_data(keys(i), retrieved_values)
-		select type (retrieved_values)
-		type is (state_data)
-			write(io,'(I20, G20.12, I20, I20, I20, G20.12, G20.12, G20.12, G20.12)') &
-			i-1, &
-			retrieved_values%time_spent/t, &
-			retrieved_values%visit_count, &
-			retrieved_values%exit_count(1), &
-			retrieved_values%exit_count(2), &
-			r_infer(i,1), &
-			Rin(i-1), &
-			r_infer(i,2), &
-			Rout(i-1)
-		end select
-	end do
+!	do i = 1, size(keys)
+!!		call set(key, keys(i))
+!		call map%get_other_data(keys(i), retrieved_values)
+!		select type (retrieved_values)
+!		type is (state_data)
+!			write(io,'(I20, G20.12, I20, I20, I20, G20.12, G20.12, G20.12, G20.12)') &
+!			i-1, &
+!			retrieved_values%time_spent/t, &
+!			retrieved_values%visit_count, &
+!			retrieved_values%exit_count(1), &
+!			retrieved_values%exit_count(2), &
+!			r_infer(i,1), &
+!			Rin(i-1), &
+!			r_infer(i,2), &
+!			Rout(i-1)
+!		end select
+!	end do
 end subroutine
 
 
